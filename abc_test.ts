d8fb306753964dd33caee513be38d9dfe73303a0
zhmushan <zhmushan@qq.com>
2018-12-14 19:28:06 +0800

    test: any handler

--- abc_test.ts
+++ abc_test.ts
@@ -1,6 +1,28 @@
-import { test } from 'https://deno.land/x/testing/testing.ts'
-import { Abc } from './abc'
+import { test, assertEqual } from 'https://deno.land/x/testing/testing.ts'
+import { Abc, NotFoundHandler } from './abc'
+import { Context } from './context'
 
-test(function testAbc() {
+const data = {
+  string: 'hello, world',
+  html: '<h1>hello, world</h1>',
+  json: { hello: 'world' }
+}
+
+const methods = ['CONNECT', 'DELETE', 'GET', 'HEAD', 'OPTIONS', 'PATCH', 'POST', 'PUT', 'TRACE']
+
+test(function testHandler() {
   const abc = new Abc()
+
+  // create a fake context
+  let ctx = new Context({} as any)
+
+  abc.any('/string', c => data.string)
+  abc.any('/html', c => data.html)
+  abc.any('/json', c => data.json)
+  for (const method of methods) {
+    assertEqual(abc.router.find(method, '/string', ctx)(ctx), data.string)
+    assertEqual(abc.router.find(method, '/html', ctx)(ctx), data.html)
+    assertEqual(abc.router.find(method, '/json', ctx)(ctx), data.json)
+    assertEqual((abc.router.find(method, '/', ctx) || NotFoundHandler), NotFoundHandler)
+  }
 })
