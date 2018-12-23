cdd1e1cc89a243e3c8087aa38cb82b64d8a8d130
zhmushan <zhmushan@qq.com>
2018-12-23 17:50:54 +0800

    WIP

--- binder_test.ts
+++ binder_test.ts
@@ -1,3 +1,27 @@
 import { t } from 'https://raw.githubusercontent.com/zhmushan/deno_test/master/index.ts'
+import { assertEqual } from 'https://deno.land/x/testing/testing.ts'
+import { binder } from './binder'
+import { context } from './context'
 
-t('bind', () => {})
+function injectContext(r = {}) {
+  return context(r as any)
+}
+
+class Obj {
+  foo?
+  bar?
+  constructor(obj = {} as Obj) {
+    this.foo = obj.foo
+    this.bar = obj.bar
+  }
+}
+
+t('bind', async () => {
+  const c = injectContext({
+    body: () => 'foo=foo',
+    headers: new Headers({ 'Content-Type': 'application/x-www-form-urlencoded' })
+  })
+  const obj = new Obj()
+  await binder().bind(obj, c)
+  assertEqual(obj, { foo: 'foo' })
+})
--- context_test.ts
+++ context_test.ts
@@ -1,8 +1,4 @@
 import { t } from 'https://raw.githubusercontent.com/zhmushan/deno_test/master/index.ts'
 import { context } from './context'
 
-export function injectContext() {
-  return context({} as any)
-}
-
 t('context', () => {})
