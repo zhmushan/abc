34f70d4ed9f4235d8053fe1bd6bd1799503c73cc
zhmushan <zhmushan@qq.com>
2018-12-23 18:11:14 +0800

    parse test

--- parser_test.ts
+++ parser_test.ts
@@ -1,3 +1,11 @@
 import { t } from 'https://raw.githubusercontent.com/zhmushan/deno_test/master/index.ts'
+import { Parser } from './parser'
+import { assertEqual } from 'https://deno.land/x/testing/testing.ts'
 
-t('parse', () => {})
+const formCases: [[string, {}]] = [[`foo=bar`, { foo: 'bar' }]]
+
+t('parse form', () => {
+  for (const c of formCases) {
+    assertEqual(Parser.form(c[0]), [c[1], undefined])
+  }
+})
