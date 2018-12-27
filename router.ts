7caaf7a7807a11767706c08a8eaa29fe6e71c962
zhmushan <zhmushan@qq.com>
2018-12-27 10:25:16 +0800

    add license

--- router.ts
+++ router.ts
@@ -1,3 +1,6 @@
+// Copyright (c) 2013 Julien Schmidt. All rights reserved.
+// Based on https://github.com/julienschmidt/httprouter
+
 import { handlerFunc } from "abc.ts";
 import { Context } from "context.ts";
 
