673075371412b683ebf8b26b6de783e4d5796f30
EDacdy <33247211+EDacdy@users.noreply.github.com>
2019-03-15 21:05:29 +0800

    update std@v0.3.2 & qs@1.0.1 (#21)

--- deps.ts
+++ deps.ts
@@ -2,9 +2,9 @@ export {
   serve,
   ServerRequest,
   Response
-} from "https://deno.land/std@v0.3.1/http/server.ts";
+} from "https://deno.land/std@v0.3.2/http/server.ts";
 export {
   Status,
   STATUS_TEXT
-} from "https://deno.land/std@v0.3.1/http/http_status.ts";
-export { parse } from "https://denolib.com/denolib/qs@v1.0.0/mod.ts";
+} from "https://deno.land/std@v0.3.2/http/http_status.ts";
+export { parse } from "https://denolib.com/denolib/qs@v1.0.1/mod.ts";
--- dev_deps.ts
+++ dev_deps.ts
@@ -1,2 +1,2 @@
-export { test, runTests } from "https://deno.land/std@v0.3.1/testing/mod.ts";
-export { assertEquals } from "https://deno.land/std@v0.3.1/testing/asserts.ts";
+export { test, runTests } from "https://deno.land/std@v0.3.2/testing/mod.ts";
+export { assertEquals } from "https://deno.land/std@v0.3.2/testing/asserts.ts";
