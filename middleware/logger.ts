ebfc1d160c129ac9b0340a19d53401737f2cdda2
木杉 <zhmushan@qq.com>
2019-05-08 10:13:23 +0800

    add middleware skipper

--- middleware/logger.ts
+++ middleware/logger.ts
@@ -1,4 +1,5 @@
 import { MiddlewareFunc, HandlerFunc, Context } from "../mod.ts";
+import { Skipper, DefaultSkipper } from "./skipper.ts";
 
 export enum LoggerFlag {
   Time = "#{time}",
@@ -7,14 +8,24 @@ export enum LoggerFlag {
 }
 
 export const DefaultLoggerConfig: LoggerConfig = {
+  skipper: DefaultSkipper,
   format: `time: '${LoggerFlag.Time}', method: '${LoggerFlag.Method}', path: '${
     LoggerFlag.Path
   }'`
 };
 
 export function logger(config = DefaultLoggerConfig): MiddlewareFunc {
+  if (config.format == null) {
+    config.format = DefaultLoggerConfig.format;
+  }
+  if (config.skipper == null) {
+    config.skipper = DefaultLoggerConfig.skipper;
+  }
   return function(next: HandlerFunc): HandlerFunc {
-    return function(c: Context): any {
+    return function(c: Context) {
+      if (config.skipper(c)) {
+        return next(c);
+      }
       let outstr = config.format;
       for (const key in LoggerFlag) {
         if (LoggerFlag[key] === LoggerFlag.Time) {
@@ -32,5 +43,6 @@ export function logger(config = DefaultLoggerConfig): MiddlewareFunc {
 }
 
 export interface LoggerConfig {
-  format: string;
+  skipper?: Skipper;
+  format?: string;
 }
---
+++ middleware/skipper.ts
@@ -0,0 +1,7 @@
+import { Context } from "../context.ts";
+
+export type Skipper = (c?: Context) => boolean;
+
+export const DefaultSkipper: Skipper = function() {
+  return false;
+};
