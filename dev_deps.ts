aaf352dcf8c3e1e6aa32d746ff9ba5e21b97ae3a
木杉 <zhmushan@qq.com>
2019-05-20 08:47:57 +0800

    upgrade deno & deno_std to v0.5.0

--- dev_deps.ts
+++ dev_deps.ts
@@ -1,7 +1,7 @@
-export { test, runTests } from "https://deno.land/std@v0.4.0/testing/mod.ts";
+export { test, runTests } from "https://deno.land/std@v0.5.0/testing/mod.ts";
 export {
   assertEquals,
   assertNotEquals,
   assertThrowsAsync,
   assertThrows
-} from "https://deno.land/std@v0.4.0/testing/asserts.ts";
+} from "https://deno.land/std@v0.5.0/testing/asserts.ts";
