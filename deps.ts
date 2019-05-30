ca0e079b958b939a091744c214819a070abcf9ff
木杉 <zhmushan@qq.com>
2019-05-30 09:24:08 +0800

    upgrade deno & deno_std to v0.7.0

--- deps.ts
+++ deps.ts
@@ -1,13 +1,13 @@
-import * as _path from "https://deno.land/std@v0.5.0/fs/path.ts";
+import * as _path from "https://deno.land/std@v0.7.0/fs/path.ts";
 
 export {
   serve,
   ServerRequest,
   Response
-} from "https://deno.land/std@v0.5.0/http/server.ts";
+} from "https://deno.land/std@v0.7.0/http/server.ts";
 export {
   Status,
   STATUS_TEXT
-} from "https://deno.land/std@v0.5.0/http/http_status.ts";
+} from "https://deno.land/std@v0.7.0/http/http_status.ts";
 export { parse } from "https://denolib.com/denolib/qs@v1.0.1/mod.ts";
 export const path = _path;
