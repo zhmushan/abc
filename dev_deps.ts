46e28bbbc8eb80a4b319979134e6e595ef372461
木杉 <zhmushan@qq.com>
2020-03-29 16:15:54 +0800

    fix examples tests

--- dev_deps.ts
+++ dev_deps.ts
@@ -6,3 +6,8 @@ export {
   assertThrows,
   assert
 } from "https://deno.land/std@v0.38.0/testing/asserts.ts";
+
+export { BufReader } from "https://deno.land/std@v0.38.0/io/bufio.ts";
+export {
+  TextProtoReader
+} from "https://deno.land/std@v0.38.0/textproto/mod.ts";
--- examples/test.ts
+++ examples/test.ts
@@ -1,35 +1,67 @@
 import { path } from "../deps.ts";
-import { assertEquals } from "../dev_deps.ts";
+import {
+  assert,
+  assertEquals,
+  TextProtoReader,
+  BufReader
+} from "../dev_deps.ts";
 const { run, test, execPath } = Deno;
 
 const dir = path.join(import.meta.url, "..");
+const addr = "http://localhost:8080";
+let server: Deno.Process;
+
+async function startServer(fpath: string): Promise<void> {
+  server = run({
+    cmd: [execPath(), "--allow-net", "--allow-read", fpath],
+    stdout: "piped",
+  });
+  assert(server.stdout != null);
+  const r = new TextProtoReader(new BufReader(server.stdout));
+  const s = await r.readLine();
+  assert(s !== Deno.EOF && s.includes("server listening"));
+}
+
+function killServer(): void {
+  server.close();
+  server.stdout?.close();
+}
 
 test(async function ExmaplesCatApp() {
-  let p = run({
-    cmd: [execPath(), "fetch", path.join(dir, "./cat_app/main.ts")],
-    stderr: "piped",
-  });
+  await startServer(path.join(dir, "./cat_app/main.ts"));
+  try {
+    const cat = { name: "zhmushan", age: 18 };
+    const createdCat = await fetch(addr, {
+      method: "POST",
+      body: JSON.stringify(cat),
+    }).then((resp) => resp.json());
+    const foundCats = await fetch(addr).then((resp) => resp.json());
+    const foundCat = await fetch(`${addr}/1`).then((resp) => resp.json());
 
-  assertEquals((await p.stderrOutput()).length, 0);
-  p.close();
+    assertEquals(createdCat, { id: 1, ...cat });
+    assertEquals(foundCat, createdCat);
+    assertEquals(foundCats, [foundCat]);
+  } finally {
+    killServer();
+  }
 });
 
 test(async function ExmaplesJSX() {
-  let p = run({
-    cmd: [execPath(), "fetch", path.join(dir, "./jsx/main.tsx")],
-    stderr: "piped",
-  });
-
-  assertEquals((await p.stderrOutput()).length, 0);
-  p.close();
+  await startServer(path.join(dir, "./jsx/main.tsx"));
+  try {
+    const text = await fetch(addr).then((resp) => resp.text());
+    assertEquals(text, `<h1 data-reactroot="">Hello</h1>`);
+  } finally {
+    killServer();
+  }
 });
 
 test(async function ExmaplesTemplate() {
-  let p = run({
-    cmd: [execPath(), "fetch", path.join(dir, "./template/main.ts")],
-    stderr: "piped",
-  });
-
-  assertEquals((await p.stderrOutput()).length, 0);
-  p.close();
+  await startServer(path.join(dir, "./template/main.ts"));
+  try {
+    const text = await fetch(addr).then((resp) => resp.text());
+    // assert(text.includes("hello, zhmushan!"));
+  } finally {
+    killServer();
+  }
 });
