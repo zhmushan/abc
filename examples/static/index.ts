9fe2428d1e741312bd92103cb10f269df15c26ab
wk989898 <wangkun989898@gmail.com>
2020-05-15 14:37:11 +0800

    middleware return

--- examples/static/index.ts
+++ examples/static/index.ts
@@ -2,10 +2,17 @@ import { Application } from "../../mod.ts";
 
 const app = new Application();
 const port = 8080;
-app.static("", "assets", (h) =>
-  (c) => {
+/**
+ * if MiddlewareFunc length more than 2
+ */
+app.static("", "assets",
+  (h) => (c):any => {
     c.response.headers.set("Access-Control-Allow-Origin", "*");
-  })
+  },
+  (h) => (c) => {
+    c.response.headers.set("Catch-Control", "no-store");
+  }
+  )
   .start({ port });
 
-console.log(`server listening on http://localhost:${port}`);
+console.log(`server listening on http://localhost:${port}\nplease add suffix from assets folder~`);
