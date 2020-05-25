eb4e0e7753c4f8e741d254c04719bd955d29f35a
Bankole Ahmed <kidrolex19@gmail.com>
2020-05-25 01:47:08 +0100

    used environment variable for port

--- examples/static/main.ts
+++ examples/static/main.ts
@@ -2,7 +2,7 @@ import { Application } from "../../mod.ts";
 import { cors } from "../../middleware/cors.ts";
 
 const app = new Application();
-const port = 8080;
+const port = process.env.PORT || 8080;
 app.static("/", "./assets", cors()).start({ port });
 
 console.log(`server listening on http://localhost:${port}`);
