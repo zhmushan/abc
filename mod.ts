8b376b4c555b69702f36cf8079bd493a4bb56364
木杉 <zhmushan@qq.com>
2020-06-01 23:09:54 +0800

    remove version.ts & bump router to 1.0.0

--- mod.ts
+++ mod.ts
@@ -2,7 +2,6 @@ export { Application } from "./app.ts";
 export { Group } from "./group.ts";
 export { Context } from "./context.ts";
 export { Router } from "./router.ts";
-export { Version } from "./version.ts";
 
 export * from "./types.ts";
 export * from "./http_exception.ts";
--- version.ts
+++
@@ -1,1 +0,0 @@
-export const Version = "v1.0.0-rc8";
