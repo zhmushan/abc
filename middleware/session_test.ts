9768e23bcce001affe8b138e0757e5eb7e33ac89
Marvin Borner <develop@marvinborner.de>
2020-08-05 11:05:34 +0200

    Added some more tests

--- middleware/session_test.ts
+++ middleware/session_test.ts
@@ -11,13 +11,28 @@ const { test } = Deno;
 const options = { app: undefined!, r: createMockRequest() };
 const c = new Context(options);
 const store = new SessionMemoryStore();
+const skey = "abc.session";
+const key = "answer";
+const value = "42";
 
 test("middleware session", function (): void {
-  c.session = new Session(store, "sessionKey");
-  c.setCookie(
-      { name: "sessionKey", value: c.session.sessionID, path: "/" },
-  );
+  c.session = new Session(store, skey);
   c.session.init();
-  c.session.set("key", "value");
-  assertEquals(c.session.get("key"), "value");
+  c.session.set(key, value);
+  assertEquals(c.session.get(key), value);
+});
+
+test("middleware session ID generator", function (): void {
+  assertEquals(c.session.sessionID, skey);
+  c.session = new Session(store);
+  c.session.init();
+  assert(c.session.sessionID.length == 64);
+});
+
+test("middleware session memory store", function (): void {
+  assert(store.sessionExists(skey));
+  assert(store.sessionExists(c.session.sessionID));
+
+  c.session.set(key, value);
+  assertEquals(store.getSession(c.session.sessionID), { [key]: value });
 });
