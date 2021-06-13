07d93ae80a19eaa1ac2c449997ecf7da5e00b018
sporadicCode <p.tsykarev@gmail.com>
2021-06-13 22:04:05 +0300

    Upgrade std to 0.98.0

--- vendor/https/deno.land/std/http/server.ts
+++ vendor/https/deno.land/std/http/server.ts
@@ -1,1 +1,1 @@
-export * from "https://deno.land/std@0.92.0/http/server.ts";
+export * from "https://deno.land/std@0.98.0/http/server.ts";
--- vendor/https/deno.land/std/testing/asserts.ts
+++ vendor/https/deno.land/std/testing/asserts.ts
@@ -1,1 +1,1 @@
-export * from "https://deno.land/std@0.92.0/testing/asserts.ts";
+export * from "https://deno.land/std@0.98.0/testing/asserts.ts";
